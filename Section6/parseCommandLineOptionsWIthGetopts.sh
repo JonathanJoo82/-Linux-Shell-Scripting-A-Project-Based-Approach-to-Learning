d38e85b1b18517cc68d491042d2b12ccc30826ea
JonathanJoo82 <jonathanjoo@gmail.com>
2024-02-21 20:18:51 -0600

    asdf

--- Section6/parseCommandLineOptionsWIthGetopts.sh
+++ Section6/parseCommandLineOptionsWIthGetopts.sh
@@ -1,27 +1,72 @@
 #!/bin/bash
 
-#getopts
-# used by shell procedures to parse positional parameters as options
+# This script generates a random password.
+# The user can set the password length with -l and add a special character with -s.
+# Verbose mode can be enabled with -v.
 
+usage() {
+  echo "Usage: ${0} [-vs] [-l LENGTH]" >&2
+  echo 'Generate a random password.' >&2
+  echo '  -l LENGTH  Specify the password length.' >&2
+  echo '  -s         Append a special character to the password.' >&2
+  echo '  -v         Increase verbosity.' >&2
+  exit 1
+}
 
-#generate a random password
+log() {
+  local MESSAGE="${@}"
+  if [[ "${VERBOSE}" = 'true' ]]
+  then
+    echo "${MESSAGE}"
+  fi
+}
 
-
-#user can set the password length with -l and add a special character with -s
-
-#verbose mode enabled with -v
-
-#set a default pw length
-
+# Set a default password length.
 LENGTH=48
+
 while getopts vl:s OPTION
-do 
-    case ${OPTION} in
+do
+  case ${OPTION} in
     v)
-        VERBOSE='true'
-        echo "Verbose mode on"
-        ;;
+      VERBOSE='true'
+      log 'Verbose mode on.'
+      ;;
     l)
-        LENGTH="${OPTARG}"
-        ::
+      LENGTH="${OPTARG}"
+      ;;
     s)
+      USE_SPECIAL_CHARACTER='true'
+      ;;
+    ?)
+      usage
+      ;;
+  esac
+done
+
+# Remove the options while leaving the remaining arguments.
+shift "$(( OPTIND - 1 ))"
+
+if [[ "${#}" -gt 0 ]]
+then
+  usage
+fi
+
+log 'Generating a password.'
+
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${LENGTH})
+
+# Append a special character if requested to do so.
+if [[ "${USE_SPECIAL_CHARACTER}" = 'true' ]]
+then
+  log 'Selecting a random special character.'
+  SPECIAL_CHARACTER=$(echo '!@#$%^&*()_-+=' | fold -w1 | shuf | head -c1)
+  PASSWORD="${PASSWORD}${SPECIAL_CHARACTER}"
+fi
+
+log 'Done.'
+log 'Here is the password:'
+
+# Display the password.
+echo "${PASSWORD}"
+
+exit 0
